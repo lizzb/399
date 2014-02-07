896def2542903b073aa75355391eaa117a1e21c4
lizzb <lizz@lizz-natalia.local>
2014-02-06 21:02:33 -0600

    Revert dc79a1b..64269df
    This rolls back to commit dc79a1be671fcd52ddf0d550f46812511b1a45c1.

--- app/angular/script.js
+++ app/angular/script.js
@@ -43,25 +43,25 @@ myApp.config(['$routeProvider','$locationProvider', function($routeProvider, $lo
 	         $routeProvider
 	         // route for the home page
 	         .when('/', {
-	           templateUrl: '/pages/home.html',
+	           templateUrl: 'pages/home.html',
 	           controller: 'mainController'
 	         })
 
 	         // route for the companies page
 	         .when('/companies', {
-	           templateUrl: '/pages/companies.html',
+	           templateUrl: 'pages/companies.html',
 	           controller: 'companiesController'
 	         })
 
 	         // route for the about page
 	         .when('/about', {
-	           templateUrl: '/pages/about.html',
+	           templateUrl: 'pages/about.html',
 	           controller: 'aboutController'
 	         })
 
 	         // route for the contact page
 	         .when('/contact', {
-	           templateUrl: '/pages/contact.html',
+	           templateUrl: 'pages/contact.html',
 	           controller: 'contactController'
 	         });
 
