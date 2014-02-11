0e6b8e438043ff0f99d3a4cd96b8fd3599ed4560
lizzb <lizz@lizz-natalia.local>
2014-02-11 16:36:34 -0600

    tried a bunch stuff for switching ascending descending icon but didn't pan out

--- app/js/filters.js
+++ app/js/filters.js
@@ -7,3 +7,18 @@ angular.module('idayFilters', []).filter('checkmark', function() {
     return input ? '\u2713' : '\u2718';
   };
 });
+
+
+/*
+<i class="fa iconname"></li>
+<i class="fa fa-sort-alpha-asc"></i>
+<i class="fa fa-sort-alpha-desc"></i>
+<i class="fa fa-sort-numeric-asc"></i>
+        <i class="fa fa-sort-numeric-desc"></i>
+        <i class="fa fa-sort"> </i>
+        <i class="fa fa-sort-asc"> </i>
+        <i class="fa fa-sort-desc"> </i>
+        <i class="fa fa-sort-amount-asc"> </i>
+        <i class="fa fa-sort-amount-desc"> </i>
+        <i class="glyphicon glyphicon-sort"></i>
+        */
