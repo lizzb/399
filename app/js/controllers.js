7450e692bbda0278bb7eb16e3a05f107c2939543
lizzb <lizz@lizz-natalia.local>
2014-02-19 11:13:37 -0600

    sort of fixed temporarily and comments taken out

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,9 +1,11 @@
 'use strict';
 
-// http://jsfiddle.net/Ln26K/
+
 /* Controllers */
+
+// http://jsfiddle.net/Ln26K/
+
 // $http.get('data/companyGrid.json').success(function(data) {
-//     $scope.companies = data;
 
 var idayControllers = angular.module('idayControllers', []);
 
@@ -11,140 +13,45 @@ idayControllers.controller('CompanyListController', ['$scope', '$http',
   function($scope, $http) {
     $http.get('data/week7grid.json').success(function(data) {
       $scope.companies = data;
-    
-      $scope.selectedMajor = [];
-
-      $scope.orderProp = 'name';
-
-      $scope.majorList = [{
-        id: 1,
-        name: 'cs'
-    }, {
-        id: 2,
-        name: 'ee'
-    }, {
-        id: 3,
-        name: 'am'
-    }];
-
-
-   $scope.setSelectedMajor = function () {
-        var id = this.major.id;
-        if (_.contains($scope.selectedMajor, id)) {
-            $scope.selectedMajor = _.without($scope.selectedMajor, id);
-        } else {
-            $scope.selectedMajor.push(id);
-        }
-        return false;
-    };
-
-    $scope.isChecked = function (id) {
-        if (_.contains($scope.selectedMajor, id)) {
-            return 'icon-ok pull-right';
-        }
-        return false;
-    };
-
-    $scope.checkAll = function () {
-        $scope.selectedMajor = _.pluck($scope.majorList, 'id');
-    };
-
-
-
-
 
     });
-
-    var categories = ['ft', 'int', 'coop', 'msphd'];
-  
-   // $scope.filters = [];
-  //$scope.selectedFilter = [];
     
-
-    
-     
-
 }]);
 
-/*
-    $scope.selectedGenres = "Action, Drama"; 
 
-$scope.containsComparator = function(expected, actual){  
-  return actual.indexOf(expected) > -1;
-};*/
-    //$scope.filterByPosition = function(category){scope.filters.push(category);}
+// http://stackoverflow.com/questions/14857739/
+// angular-js-detailed-view-with-only-one-json
 
-    //http://jsfiddle.net/TahmidTanzim/N9Vqk/
-    //  ($scope) {
-    /*$scope.setSelectedFilter = function (input) {
-        //var id = this.company.id;
-        var id = input;
-        if (_.contains($scope.selectedFilter, id)) {
-            $scope.selectedFilter = _.without($scope.selectedFilter, id);
-        } else {
-            $scope.selectedFilter.push(id);
-        }
-        return false;
-    };*/
-
-    /*$scope.isChecked = function (id) {
-        if (_.contains($scope.selectedFilter, id)) {
-            return 'icon-ok pull-right';
-        }
-        return false;
-    };*/
-  /*}]);*/
-
-
-/*
-$http.get('phones/full-phones.json').success(function(data){
-
-phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams',
-  function($scope, $routeParams) {
-    $scope.phoneId = $routeParams.phoneId;
-  }]);
-*/
 
 // positive theres a more efficient way to do this....
-// $http.get('data/companyGrid.json').success(function(data){
-  // if (item.booth == $routeParams.companyId)
+// should load full list once, store it
+// store a currently filtered verison
+// and just pass the relevant item from the list of companies
+// to the scope of the details
+
+
+// need a factory i think but services were being weird
+/*
+idayControllers.controller('CompanyDetailsController', ['$scope', '$routeParams',
+  function($scope, $routeParams){
+   // $scope.company = $routeParams.companyId;
+  }])*/
 
 idayControllers.controller('CompanyDetailsController', ['$scope', '$routeParams', '$http',
   function($scope, $routeParams, $http) {
     $http.get('data/week7grid.json').success(function(data){
         
         angular.forEach(data, function(comp) {
+          // if (item.booth == $routeParams.companyId)
           if (comp.id == $routeParams.companyId) 
           {
             $scope.company = comp;
             // $scope.phoneId = $routeParams.phoneId;
-            //$scope.mainImageUrl = item.logoFull;
           }    
         });
-
-        /*
-        $scope.setImage = function(imageUrl) {
-      $scope.mainImageUrl = imageUrl;
-       
-      }*/
     });
      
   }]);
-
-
-idayControllers.controller('CompanyListLocationController', ['$scope', '$routeParams',
-  function($scope, $routeParams) {
-    //$scope.phoneId = $routeParams.phoneId;
-  }]);
-
-idayControllers.controller('CompanyLocationController', ['$scope', '$routeParams',
-  function($scope, $routeParams) {
-    //$scope.companyId = $routeParams.phoneId;
-  }]);
-
-// http://stackoverflow.com/questions/14857739/angular-js-detailed-view-with-only-one-json
-
-
 
 
 /*
@@ -157,18 +64,42 @@ Use an Angular filter on your view
     <p>lon: {{entry.lon}}</p>
 </div>
 
-	http://jsfiddle.net/bmleite/Ad6u9/
- 	
+  http://jsfiddle.net/bmleite/Ad6u9/
+  
 And if you want to filter your view on a $routeParams parameter, 
 add this to your controller: 
 $scope.myParam = $routeParams.parameterNameFromRouteController; 
-and then in your view,
-modify the filter to be: 
+
+and then in your view, modify the filter to be: 
 <div ng-repeat="entry in playgrounds | filter:{id: myParam}"> 
 <p>properties: {{entry.properties}}</p> 
-<p>lat: {{entry.lat}}</p> 
-<p>lon: {{entry.lon}}</p> 
+<p>lat: {{entry.lat}}</p><p>lon: {{entry.lon}}</p> 
 </div> –  Matty J Feb 27 '13 at 3:31
-
-
 */
+
+/* ORIGINAL FROM PHONECAT for phone details
+
+$http.get('phones/full-phones.json').success(function(data){
+phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams',
+  function($scope, $routeParams) {
+    $scope.phoneId = $routeParams.phoneId;
+  }]);
+*/
+
+
+idayControllers.controller('CompanyListLocationController', ['$scope', '$routeParams',
+  function($scope, $routeParams) {
+    //$scope.phoneId = $routeParams.phoneId;
+  }]);
+
+idayControllers.controller('CompanyLocationController', ['$scope', '$routeParams',
+  function($scope, $routeParams) {
+    //$scope.companyId = $routeParams.phoneId;
+  }]);
+
+
+
+
+
+
+
--- app/js/filters.js
+++ app/js/filters.js
@@ -2,7 +2,8 @@
 
 /* Filters */
 
-// Define a new module for our app. The array holds the names of dependencies if any.
+// Define a new module for our app. 
+// The array holds the names of dependencies if any.
 
 var app = angular.module('idayFilters', []);
 
@@ -12,106 +13,14 @@ app.filter('checkmark', function() {
   };
 });
 
-app.filter('majorFilter', function () {
-    return function (companies, selectedMajor) {
-        if (!angular.isUndefined(companies) && !angular.isUndefined(selectedMajor) && selectedMajor.length > 0) {
-            var tempCompanies = [];
-            angular.forEach(selectedMajor, function (id) {
-                // need to iterate through a list not just 1
-                angular.forEach(companies, function (company) {
-                    if (angular.equals(company.major.id, id)) {
-                        tempCompanies.push(company);
-                    }
-                });
-            });
-            return tempCompanies;
-        } else {
-            return companies;
-        }
-    };
-});
-
-/*
-app.filter('majorFilter', [function () {
-    return function (companies, selectedMajor) {
-        if (!angular.isUndefined(companies) && !angular.isUndefined(selectedMajor) && selectedMajor.length > 0) {
-            var tempCompanies = [];
-            angular.forEach(selectedMajor, function (id) {
-                angular.forEach(companies, function (company) {
-                    if (angular.equals(company.major.id, id)) {
-                        tempCompanies.push(company);
-                    }
-                });
-            });
-            return tempCompanies;
-        } else {
-            return companies;
-        }
-    };
-}]);
-*/
 
 // http://tutorialzine.com/2013/08/learn-angularjs-5-examples/
-    // All filters must return a function. The first parameter
-    // is the data that is to be filtered, and the second is an
-    // argument that may be passed with a colon (searchFor:searchString) filterPostion:position
-/*app.filter('filterByPositionv', function(){
 
-return function(arr, positionFilter){
+// All filters must return a function.
+// The first parameter is the data that is to be filtered, 
+// and the second is an argument that may be passed 
+// with a colon (searchFor:searchString) filterPostion:position
 
-        if(!positionFilter){
-            return arr;
-        }
 
-        var result = []; // filtered array
-
-        //positionFilter = positionFilter.toLowerCase();
-
-        // Using the forEach helper method to loop through the array
-        angular.forEach(arr, function(company){
-
-            if(company.position.indexOf(positionFilter) !== -1){
-                result.push(company);
-            }
-
-        });
-
-        return result;
-    };
-
-});*/
-
-/*
-<i class="fa iconname"></li>
-<i class="fa fa-sort-alpha-asc"></i>
-<i class="fa fa-sort-alpha-desc"></i>
-<i class="fa fa-sort-numeric-asc"></i>
-        <i class="fa fa-sort-numeric-desc"></i>
-        <i class="fa fa-sort"> </i>
-        <i class="fa fa-sort-asc"> </i>
-        <i class="fa fa-sort-desc"> </i>
-        <i class="fa fa-sort-amount-asc"> </i>
-        <i class="fa fa-sort-amount-desc"> </i>
-        <i class="glyphicon glyphicon-sort"></i>
-        */
-
-        /*
-
-        http://jsfiddle.net/TahmidTanzim/N9Vqk/
-angular.module('idayFilters', []).filter('positionFilter', [function () {
-    return function (companies, selectedFilter) {
-        if (!angular.isUndefined(companies) && !angular.isUndefined(selectedFilter) && selectedFilter.length > 0) {
-            var filteredCompanies = [];
-            angular.forEach(selectedFilter, function (id) {
-                /*angular.forEach(companies, function (companies) {
-                    if (angular.equals(client.company.id, id)) {*
-                        filteredCompanies.push(client);
-                    //}
-                });
-            });
-            return filteredCompanies;
-        } else {
-            return companies;
-        }
-    };
-}]);*/
+// http://jsfiddle.net/TahmidTanzim/N9Vqk/
+// company and client with underscore example
