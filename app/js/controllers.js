ec20c4b213e63e4d878c1506424d816feeb7e5a2
lizzb <lizz@lizz-natalia.local>
2014-02-23 18:30:53 -0600

    ITS UGLY BUT IT WORKS aaaahahhhhhhhhhhhhhhh
    okay now forrealz i have to stop like seriously, seriously.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -10,8 +10,8 @@ var idayControllers = angular.module('idayControllers', []);
 
 //http://jsfiddle.net/natefriedman/3XT3F/1/
 
-idayControllers.controller('CompanyListController', ['$scope', '$http', '$rootScope', '$filter',
-  function($scope, $http, $rootScope, $filter) {
+idayControllers.controller('CompanyListController', ['$scope', '$http', '$rootScope', //'$filter',
+  function($scope, $http, $rootScope){ //$filter) {
 
 $rootScope.positions = [ //$scope.positions = [
   { friendlyName: 'Full Time', name: 'fte'},
@@ -45,9 +45,57 @@ $rootScope.positions = [ //$scope.positions = [
 });
 
 
+    $scope.majorsIncluded = [];
+    $scope.positionsIncluded = [];
+
+    $scope.includeMajor = function (color) {
+        var i = $scope.majorsIncluded.indexOf(color);
+        if (i > -1) { $scope.majorsIncluded.splice(i, 1); } 
+        else { $scope.majorsIncluded.push(color); }
+    }
+    
+     $scope.includePosition = function (color) {
+        var i = $scope.positionsIncluded.indexOf(color);
+        if (i > -1) { $scope.positionsIncluded.splice(i, 1); } 
+        else { $scope.positionsIncluded.push(color); }
+    }
 
 
-      $filter('companyFilter')($rootScope.majors, $rootScope.positions);
+$scope.userFilter = function (company) {
+        
+        var includedPositions= $scope.positionsIncluded;
+        var includedMajors = $scope.majorsIncluded;
+        
+        if($scope.majorsIncluded.length <= 0 && $scope.positionsIncluded.length <= 0)
+            return company;
+        
+        if($scope.positionsIncluded.length <=0) includedPositions = ["fte","intern","coop","msphd"];
+            
+        
+        if($scope.majorsIncluded.length <=0)
+    includedMajors = ["am","bme","chem","civil","ce","cs","ee","enve","ie","made","matsci","mech","noneng"];
+        
+        //if(includedPositions.length <= 0) includedPositions= $scope.positions;
+        //if(includedMajors.length <= 0) includedMajors = $scope.majors;
+        
+        for (var i = 0; i < includedPositions.length; i++)
+        {
+            // check if a position is true
+            if(company[includedPositions[i]])
+            {
+                // must have at least one of these true as well
+                for (var j = 0; j < includedMajors.length; j++)
+                {
+                    if(company[includedMajors[j]]) return company;
+                }
+            }
+            
+        }
+        return;
+    }
+
+
+     // $filter('companyFilter')($rootScope.majors, $rootScope.positions);
     
 
 /*
